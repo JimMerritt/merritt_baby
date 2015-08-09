cebc18028ace6e1987a559415c97233b86b8162d
JimMerritt <merritt.ja@gmail.com>
2015-08-09 13:50:16 -0400

    Updated styles for home page and made generic layout pieces for other pages.

---
+++ app/assets/javascripts/messages_scrolling.js
@@ -0,0 +1,24 @@
+function startScroll() {
+  var slides = [];
+  var count;
+  $("#scrolling ul li").each(function () {
+    slides.push($(this).text())
+  });
+  var maxCount = slides.length;
+  
+  function marginShift() {
+    count = 1;
+    while (count < maxCount) {
+      var marginChange = count * 100;
+      var x = "-" + marginChange.toString() + "%";
+      $("#scrolling ul").animate({
+        marginLeft: x,
+      }, 4000).delay(1000);
+      count++;
+    }
+  }
+  
+  for(i=0; i < 5; i++) {
+    marginShift();
+  }
+}
